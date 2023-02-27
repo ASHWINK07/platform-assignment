a2b4d0db3f9ab0109c1cdc46727d768b4f1203ab
ASHWINK07 <ashwinkachu07@gmail.com>
2023-02-27 17:09:59 +0530

    feat(added-RestAPi-for-mongodb-service): Get request,Put request, Post request, Delete Request added for mongo service ,database still not configured
    BREAKING CHANGE:

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -19,30 +19,56 @@ public class DemoApplication {
 	}
 	@ResponseBody
     @GetMapping(value = "/mysql")
-    public String getdetails(@RequestParam String name){
+    public String getdetailssql(@RequestParam String name){
 		//String name = 
-		System.out.println("Get request successfull ");
+		System.out.println("Get request successfull for mysql ");
         return name;
     }
 	@ResponseBody
 	@DeleteMapping(value = "/mysql")
-	public String deletedetails(@RequestParam String name){
-		System.out.println("Delete request successfull");
+	public String deletedetailssql(@RequestParam String name){
+		System.out.println("Delete request successfull for mysql ");
 		return name;
 	}
 	@ResponseBody
 	@PostMapping(value = "/mysql")
-	public String insertdetails(@RequestParam("name") String name,@RequestParam("department") String department){
-		System.out.println("Post Request Successfull");
+	public String insertdetailssql(@RequestParam("name") String name,@RequestParam("department") String department){
+		System.out.println("Post Request Successfull for mysql ");
 		return "name: "+name+"&department:"+department;
 	}
 	@ResponseBody
 	@PutMapping(value = "/mysql")
-	public String updatedetails(@RequestParam("name") String name,@RequestParam("department") String department){
-		System.out.println("Put Request Successfull");
+	public String updatedetailssql(@RequestParam("name") String name,@RequestParam("department") String department){
+		System.out.println("Put Request Successfull for mysql ");
 		return "name: "+name+"&department:"+department;
 	}
 
+	@ResponseBody
+	@GetMapping(value = "/mongodb")
+	public String getdetailsmongodb(@RequestParam String name){
+		System.out.println("Get request successfull for mongodb ");
+        return name;	
+	}
 
+	@ResponseBody
+	@DeleteMapping(value="/mongodb")
+	public String deletedetailsmongodb(@RequestParam String name){
+		System.out.println("Delete Request successfull for mongodb");
+		return name;
+	}
+
+	@ResponseBody
+	@PostMapping(value = "/mongodb")
+	public String insertdetailsmongodb(@RequestParam("name") String name,@RequestParam("department") String department){
+		System.out.println("Post Request sucessfull for mongodb");
+		return "name: "+name+"&department:"+department;
+	}
+
+	@ResponseBody
+	@PutMapping(value = "/mongodb")
+	public String updatedetailsmongodb(@RequestParam("name") String name,@RequestParam("department") String department){
+		System.out.println("Put Request sucessfull for mongodb");
+		return "name: "+name+"&department:"+department;
+	}
 
 }
