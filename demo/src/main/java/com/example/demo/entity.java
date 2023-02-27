9b6a1b625d346197afc2c5899aac15833ed260e5
ASHWINK07 <ashwinkachu07@gmail.com>
2023-02-27 21:16:36 +0530

    feat(all-rest-apis-added): all rest apis done in java
    BREAKING CHANGE:

---
+++ demo/src/main/java/com/example/demo/entity.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+ 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+public class entity {
+    
+}
