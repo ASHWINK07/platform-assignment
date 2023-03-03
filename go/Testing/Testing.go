acfe90e2d8d34ab00a577168f306fc9c9285e101
ASHWINK07 <ashwinkachu07@gmail.com>
2023-03-03 10:45:15 +0530

    refactor(refactored-folder-names): refactored folder names

--- go/Testing/Testing.go
+++ go/Testing/Testing.go
@@ -6,6 +6,9 @@ import (
 )
 
 func Testing(w http.ResponseWriter, r *http.Request) {
+	//print testing on the client-side
+	fmt.Fprintf(w, "test successfull")
+	//print testing on server side
 	fmt.Println("testing")
 	return
 }
